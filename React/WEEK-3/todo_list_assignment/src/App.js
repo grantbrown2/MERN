acf89e85100f0c6aad3237cc8a19853a76f9615a
grantbrown2 <gbrownzzzz58@gmail.com>
2023-02-24 01:25:28 -0500

    week-3-day-3 progress

---
+++ React/WEEK-3/todo_list_assignment/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
