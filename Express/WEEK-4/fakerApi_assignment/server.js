8bef2511def9f08d41b0f6eff865e56762fc8533
grantbrown2 <gbrownzzzz58@gmail.com>
2023-03-04 16:51:38 -0500

    week-4-day-3 content update

--- Express/WEEK-4/fakerApi_assignment/server.js
+++
@@ -1,7 +0,0 @@
-const {faker} = require('faker');
-const express = require('express');
-
-const app = express();
-const port = 8000;
-
-app.listen(port, () => console.log(`Listening on port: ${port}`));
